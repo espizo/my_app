ea3ac7af83a4adcdc80c513d5c5e2b31abc7b4fe
espizo <espen.borgen@startsiden.no>
2016-02-10 12:27:49 +0700

    Add working shit

--- lib/MyApp/Controller/Pet.pm
+++ lib/MyApp/Controller/Pet.pm
@@ -1,35 +1,22 @@
 package MyApp::Controller::Pet;
 use Mojo::Base 'Mojolicious::Controller';
 
-our $RES = [{id => 123, name => 'kit-cat'}];
-our $CODE = 200;
+our $PETS = [
+    { id => 123, name => 'cat' },
+    { id => 456, name => 'dog' },
+];
 
 sub list {
-  my ($c, $args, $cb) = @_;
-  $c->$cb($RES, $CODE);
+  my ( $self, $args, $cb ) = @_;
+  $self->$cb( $PETS, 200 );
 }
+
 
 sub show {
-  my ($c, $args, $cb) = @_;
-  $RES->{id} = $args->{petId};
-  $c->$cb($RES, $CODE);
-}
-
-sub get {
-  my ($c, $args, $cb) = @_;
-  return $c->$cb('', 201) if $CODE eq '201';
-  return $c->$cb($RES, $CODE);
-}
-
-sub add {
-  my ($c, $args, $cb) = @_;
-  $RES->{name} = $args->{data}{name} if ref $args->{data} eq 'HASH';
-  $c->$cb($RES, $CODE);
-}
-
-sub update {
-  my ($c, $args, $cb) = @_;
-  $c->$cb($RES, $CODE);
+  my ( $self, $args, $cb ) = @_;
+  my ( $entry ) = grep { $_->{id} == $args->{id} } @$PETS;
+  return $self->$cb( $entry, 200 ) if $entry;
+  return $self->$cb( { errors => [ { message => 'Pet not found.', path => '/id' } ] }, 404 );
 }
 
 1;
